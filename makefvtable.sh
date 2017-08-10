2e474ac4f98267761a0a3912b79ade04ab143de2
Joshua E. Roybal <oysbharljo@yahoo.com>
2017-08-10 14:10:29 -0500

    Update makefvtable.sh

--- makefvtable.sh
+++ makefvtable.sh
@@ -2,5 +2,5 @@
 set -v
 gcc -O2 -c fvtable.c
 gcc -lm -o fvtable.cgi fvtable.o
-sudo cp fvtable.cgi /home/http/cgi-bin
-sudo chown apache:apache /home/http/cgi-bin/fvtable.cgi
+sudo cp fvtable.cgi /srv/httpd/cgi-bin
+sudo chown apache:apache /srv/httpd/cgi-bin/fvtable.cgi
